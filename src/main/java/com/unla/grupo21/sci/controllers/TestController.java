48bee20267c9b76e89846e6ce472db4a48e84b35
Leandro Blandi <59542421+leandroblandi@users.noreply.github.com>
2024-05-09 20:12:14 -0300

    [MAIN] Primeros avances en develop: configuraciones (#2)
    * feat(app): Agrego configuraciones
    
    Se agrega dependencia del módulo de Spring Security, así como también una clase de configuración.
    Asimismo, se agrega un Service para cargar los usuarios de la BBDD.
    Por ultimo, se agrega un Helper para indicar las rutas del backend.
    
    * Refactor: Mejora de URL de la base de datos (#1)
    
    Para no pisarnos entre nosotros, con respectos a los puertos.
    
    ---------
    
    Co-authored-by: Alejo Gaston Brites <115959555+AlejoBrites@users.noreply.github.com>

--- src/main/java/com/unla/grupo21/sci/controllers/TestController.java
+++ src/main/java/com/unla/grupo21/sci/controllers/TestController.java
@@ -1,11 +1,11 @@
 package com.unla.grupo21.sci.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class TestController {
 	@GetMapping("/status")
---
+++ src/main/java/com/unla/grupo21/sci/helpers/ViewRouteHelper.java
@@ -0,0 +1,8 @@
+package com.unla.grupo21.sci.helpers;
+
+/**
+ * Clase de ayuda para indicar las diferentes rutas de nuestra aplicación
+ */
+public abstract class ViewRouteHelper {
+	public final static String INDEX = "home/index";
+}
