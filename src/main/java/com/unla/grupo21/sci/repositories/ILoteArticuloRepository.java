fdcaf85a40829a7ea614d6b60e457e3e0c43717e
Alejo Gaston Brites <alejobrites456@gmail.com>
2024-05-10 15:04:04 -0300

    feat: agregado de repositorios

---
+++ src/main/java/com/unla/grupo21/sci/repositories/ILoteArticuloRepository.java
@@ -0,0 +1,11 @@
+package com.unla.grupo21.sci.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unla.grupo21.sci.entities.Usuario;
+
+@Repository
+public interface ILoteArticuloRepository extends JpaRepository<Usuario, Long> {
+
+}
