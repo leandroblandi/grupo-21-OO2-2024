fa2555229a6b8be75b78a7dda9b44edb8b747261
Alejo Gaston Brites <alejobrites456@gmail.com>
2024-05-10 15:09:30 -0300

    feat: correccion en el repositorio LoteArticulo

--- src/main/java/com/unla/grupo21/sci/repositories/ILoteArticuloRepository.java
+++ src/main/java/com/unla/grupo21/sci/repositories/ILoteArticuloRepository.java
@@ -3,9 +3,9 @@ package com.unla.grupo21.sci.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.unla.grupo21.sci.entities.Usuario;
+import com.unla.grupo21.sci.entities.LoteArticulo;
 
 @Repository
-public interface ILoteArticuloRepository extends JpaRepository<Usuario, Long> {
+public interface ILoteArticuloRepository extends JpaRepository<LoteArticulo, Long> {
 
 }
