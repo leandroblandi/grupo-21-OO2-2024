8ceb2eebfaed3e47cac77d24c2491d2f34a1d7f5
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2024-06-10 22:28:30 -0300

    fix: Organizo imports

--- src/main/java/com/unla/grupo21/sci/entities/Usuario.java
+++ src/main/java/com/unla/grupo21/sci/entities/Usuario.java
@@ -2,13 +2,9 @@ package com.unla.grupo21.sci.entities;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.unla.grupo21.sci.enums.Rol;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
