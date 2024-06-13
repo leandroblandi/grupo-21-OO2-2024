c207ca2734e84508dfdab0ba9977a327c30959ce
Alejo Gaston Brites <alejobrites456@gmail.com>
2024-06-13 19:46:02 -0300

    feat: algunos cambios

--- src/main/java/com/unla/grupo21/sci/controllers/ArticuloController.java
+++ src/main/java/com/unla/grupo21/sci/controllers/ArticuloController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unla.grupo21.sci.dtos.CrearArticuloDto;
 import com.unla.grupo21.sci.entities.Articulo;
 import com.unla.grupo21.sci.services.IArticuloService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +34,13 @@ public class ArticuloController {
 	public ResponseEntity<Articulo> traerArticulo(@PathVariable Long id) {
 		Articulo articulo = service.traerArticulo(id);
 		return ResponseEntity.ok(articulo);
+	}
+	
+	@PostMapping("/articulos")
+	public ResponseEntity<Articulo> crearArticulo(@Valid @RequestBody CrearArticuloDto articuloDto){
+		Articulo articulo = Articulo.builder().descripcion(articuloDto.getDescripcion()).costo(articuloDto.getCosto())
+				.precioVenta(articuloDto.getPrecioVenta()).build();
+		
+		return ResponseEntity.ok(service.crearArticulo(articulo));
 	}
 }
---
+++ src/main/java/com/unla/grupo21/sci/dtos/CrearArticuloDto.java
@@ -0,0 +1,19 @@
+package com.unla.grupo21.sci.dtos;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CrearArticuloDto {
+
+	@Length(min = 4, max = 128)
+	private String descripcion;
+
+	private double costo;
+
+	private double precioVenta;
+
+}
