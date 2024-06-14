d9eeddb1357846b8192a70a8531c86adbe553de3
Alejo Gaston Brites <alejobrites456@gmail.com>
2024-06-14 15:07:08 -0300

    feat: agrego logica para traer y crear roles

---
+++ src/main/java/com/unla/grupo21/sci/services/IUsuarioRolService.java
@@ -0,0 +1,17 @@
+package com.unla.grupo21.sci.services;
+
+import java.util.List;
+
+import com.unla.grupo21.sci.entities.UsuarioRol;
+
+public interface IUsuarioRolService {
+
+	public List<UsuarioRol> traerUsuarioRol();
+	
+	public UsuarioRol traerUsuarioRol(int id);
+	
+	public UsuarioRol traerUsuarioRol(String nombre);
+	
+	public UsuarioRol crearUsuarioRol(String nombre);
+	
+}
